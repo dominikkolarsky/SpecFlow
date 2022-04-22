8f074b20cd8fe439cf82b582a4864350faabde12
Dom <kolarskydominik@gmail.com>
2022-04-22 13:26:27 +0200

    initialion

--- SpecFlowProject/StepDefinitions/MessagesStepDefinitions.cs
+++ SpecFlowProject/StepDefinitions/MessagesStepDefinitions.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,10 +8,16 @@ namespace SpecFlowProject.StepDefinitions
     [Binding]
     public class MessagesStepDefinitions
     {
+        IWebDriver? driver;
+        IWebElement? linkMessages;
+
         [Given(@"I Have opened the website")]
         public void GivenIHaveOpenedTheWebsite()
         {
-            throw new PendingStepException();
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://certicon-testing.azurewebsites.net/");
+            linkMessages = driver.FindElement(By.LinkText("Messages"));
+            linkMessages.Click();
         }
 
         [Given(@"I have entered a name")]

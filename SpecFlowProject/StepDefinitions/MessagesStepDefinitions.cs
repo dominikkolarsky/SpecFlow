7be26385b9613306e4c8613adb9f9bac23a75c4b
Dom <kolarskydominik@gmail.com>
2022-04-22 13:35:16 +0200

    finalle testy na vse

--- SpecFlowProject/StepDefinitions/MessagesStepDefinitions.cs
+++ SpecFlowProject/StepDefinitions/MessagesStepDefinitions.cs
@@ -3,17 +3,26 @@ using OpenQA.Selenium.Chrome;
 using System;
 using TechTalk.SpecFlow;
 
-namespace SpecFlowProject.StepDefinitions
-{
+namespace SpecFlowProject.StepDefinitions {
     [Binding]
-    public class MessagesStepDefinitions
-    {
+    public class MessagesStepDefinitions {
         IWebDriver? driver;
         IWebElement? linkMessages;
+        private IWebElement? tbName;
+        private IWebElement? tbContent;
+        private IWebElement? tbEmail;
+        private IWebElement? buttonCreate;
+        private IWebElement? succesInfoLabel;
+        private IWebElement? messageNumberLabel;
+
+        [BeforeScenario]
+        public void Before() {
+            driver = new ChromeDriver();
+        }
+
 
         [Given(@"I Have opened the website")]
-        public void GivenIHaveOpenedTheWebsite()
-        {
+        public void GivenIHaveOpenedTheWebsite() {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://certicon-testing.azurewebsites.net/");
             linkMessages = driver.FindElement(By.LinkText("Messages"));
@@ -21,33 +30,40 @@ namespace SpecFlowProject.StepDefinitions
         }
 
         [Given(@"I have entered a name")]
-        public void GivenIHaveEnteredAName()
-        {
-            throw new PendingStepException();
+        public void GivenIHaveEnteredAName() {
+            tbName = driver.FindElement(By.Id("Name"));
+            tbName.SendKeys("Karel");
         }
 
         [Given(@"I have entered a correct email")]
-        public void GivenIHaveEnteredACorrectEmail()
-        {
-            throw new PendingStepException();
+        public void GivenIHaveEnteredACorrectEmail() {
+            tbEmail = driver.FindElement(By.Id("Email"));
+            tbEmail.SendKeys("Karel@mail.com");
         }
 
         [Given(@"I have entered some message")]
-        public void GivenIHaveEnteredSomeMessage()
-        {
-            throw new PendingStepException();
+        public void GivenIHaveEnteredSomeMessage() {
+            tbContent = driver.FindElement(By.Id("Content"));
+            tbContent.SendKeys("Just another test fellas.");
         }
 
         [When(@"I click the Creat button")]
-        public void WhenIClickTheCreatButton()
-        {
-            throw new PendingStepException();
+        public void WhenIClickTheCreatButton() {
+            buttonCreate = driver.FindElement(By.Id("buttonCreate"));
+            buttonCreate.Click();
         }
 
         [Then(@"The success message should be displayed")]
-        public void ThenTheSuccessMessageShouldBeDisplayed()
-        {
-            throw new PendingStepException();
+        public void ThenTheSuccessMessageShouldBeDisplayed() {
+            succesInfoLabel = driver.FindElement(By.Id("success"));
+            Assert.AreEqual("The message has been saved", succesInfoLabel.Text);
+            messageNumberLabel = driver.FindElement(By.Id("messageNumber"));
+            Assert.AreEqual("You have 1 messages", messageNumberLabel.Text);
+        }
+
+        [AfterScenario]
+        public void CleanUp() {
+            driver.Close();
         }
     }
 }

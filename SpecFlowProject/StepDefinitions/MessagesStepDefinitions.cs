65967e308864f6c195bc51c7dfbc7776edb4b22e
Dom <kolarskydominik@gmail.com>
2022-04-22 13:11:35 +0200

    scenario for test

---
+++ SpecFlowProject/StepDefinitions/MessagesStepDefinitions.cs
@@ -0,0 +1,45 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.StepDefinitions
+{
+    [Binding]
+    public class MessagesStepDefinitions
+    {
+        [Given(@"I Have opened the website")]
+        public void GivenIHaveOpenedTheWebsite()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have entered a name")]
+        public void GivenIHaveEnteredAName()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have entered a correct email")]
+        public void GivenIHaveEnteredACorrectEmail()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have entered some message")]
+        public void GivenIHaveEnteredSomeMessage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the Creat button")]
+        public void WhenIClickTheCreatButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The success message should be displayed")]
+        public void ThenTheSuccessMessageShouldBeDisplayed()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
